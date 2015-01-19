0173f105741bbf569decad29af1383c71485654c
perrettc <perrette.c@gmail.com>
2015-01-19 12:48:29 +0100

    day1

--- src/mem.c
+++ src/mem.c
@@ -15,6 +15,41 @@ void *zone_memoire = 0;
 
 /* ecrire votre code ici */
 
+// Erreurs
+#define PLUS_DE_MEMOIRE -1
+#define TAILLE_TROP_GRANDE -2
+
+//TZL : contient des pointeurs vers les listes de zone libre
+void* TZL[BUDDY_MAX_INDEX + 1];
+
+// s_header zone d'entête contenant les informations de la zone libre 
+// et un pointeur vers la zone libre suivante
+typedef struct
+{
+  void* suiv;
+} s_header;
+
+unsigned int log2(unsigned long puiss)
+{
+  unsigned int log = 0;
+  while(puiss != 1) {
+    puiss = puiss / 2;
+    log++;
+  }
+
+  return log;
+}
+
+/* Fonction retournant l'adresse d'un compagnon d'une zone memoire */
+void*
+getBuddy(void* origin, unsigned long size)
+{
+  //on aligne notre zone memoire sur l'adresse 0
+  void* origin_normalisee = origin - zone_memoire;
+
+   
+}
+
 int 
 mem_init()
 {
@@ -27,6 +62,17 @@ mem_init()
     }
 
   /* ecrire votre code ici */
+
+  /* Creation de la zone libre */
+  //mise du pointeur suivant à null
+  ((s_header)(*zone_memoire)).suiv = NULL;
+
+  /* init TZL */
+  for(int i = 0; i < BUDDY_MAX_INDEX; i++)
+    TZL[i] = NULL;
+
+  /* ajout de la zone libre dans TZL */
+  TZL[BUDDY_MAX_INDEX] = zone_memoire;
 
   return 0;
 }
@@ -49,8 +95,6 @@ mem_free(void *ptr, unsigned long size)
 int
 mem_destroy()
 {
-  /* ecrire votre code ici */
-
   free(zone_memoire);
   zone_memoire = 0;
   return 0;
